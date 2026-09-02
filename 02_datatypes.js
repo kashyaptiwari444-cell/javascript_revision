c4e4315ba207bb9d78e34f1d7f3ad36e3dea9ed6
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-02 17:18:26 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as never version.
+
+// alert( 3 + 3 ) // we are use nodejs , not browser.
+
+console.log("hello"+ 
+    1
+    +
+    1); // code readability should be high.
+
+    console.log("kashayp");
+
+let name = "kashyap"
+let age = 18
+let isLoggedIn = false
+
+/*
+Types of datatype--
+
+number = 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbols => 
+
+object
+
+*/
+
+console.log(typeof age); 
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
