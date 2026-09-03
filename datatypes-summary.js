7e96878345ff26e93f29d1ae93c305dfdaae45f3
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-03 18:41:22 +0000

    memory- heap, stack

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -2,6 +2,7 @@
 
 // Primitive
 //      7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+//      jo variable declare kra uska ak copy milta hai.
 
 const score = 100
 const scoreValue = 100.3
@@ -24,6 +25,7 @@ const bigNumber = 827982349845680283085n  // BigInt
 
 // Referance (Non primitive)
 //      Array, Objects, Functions
+//      Original value ka referance.
 
 const heros = ["thor", "spiderman", "naagraj"]
 let myObj = {
@@ -42,3 +44,33 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction); // datatype - function
+
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// memory is 2 types
+// Stack (Primitive) , Heap(Non-Primitive)
+
+let myYoutubename = "kashyaptiwaribca07"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "kash@google.com",
+    upi: "kt@1023"   
+}
+
+let userTwo = userOne
+
+userTwo.email = "tiwari@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
