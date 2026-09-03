d2537e0d93e01b4e13e9b590226cdec4f4dbe8bd
Kashyap Tiwari <kashyaptiwari444@gmail.com>
2026-09-03 16:52:59 +0000

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "kash"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kash" => true
+
+
+let someNumber = 44
+
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
